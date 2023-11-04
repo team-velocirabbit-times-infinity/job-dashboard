4b65fee413774a6b327c60b276cde2313ab0e320
Andy Lam <axm.lam@gmail.com>
2023-11-04 16:01:25 -0700

    completed basic create and get all listings functionality

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -1,7 +1,64 @@
-// const db = require('../models/listingModel');
+const db = require('../models/listingModel');
 
 const listingController = {};
 
-listingController.getListings = (req, res, next) => {};
+listingController.getAllListings = (req, res, next) => {
+  const listingsQuery = `
+  SELECT * FROM listings
+  `;
+  db.query(listingsQuery)
+    .then((listingsData) => {
+      console.log(listingsData.rows);
+      res.locals.listings = listingsData.rows;
+      return next();
+    })
+    .catch((err) => {
+      console.log(err)
+      return next(err);
+    });
+};
+
+listingController.addListing = (req, res, next) => {
+  const {
+    title,
+    company,
+    level,
+    hours,
+    minSalary,
+    maxSalary,
+    location,
+    // status,
+    url,
+    // userId,
+  } = req.body;
+  const newListingQuery = `
+  INSERT INTO listings (title, company, level, hours, "minSalary", "maxSalary", location, status, url, "userId")
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+  `;
+
+  // default status is not started. userId hardcoded to 1 for now
+  const params = [
+    title,
+    company,
+    level,
+    hours,
+    minSalary,
+    maxSalary,
+    location,
+    'Not started',
+    url,
+    1,
+  ];
+
+  db.query(newListingQuery, params)
+    .then((data) => {
+      res.locals.newListing = data.rows[0];
+      return next();
+    })
+    .catch((err) => {
+      console.log(err);
+      return next(err);
+    });
+};
 
 module.exports = listingController;
--- server/routes/listing.js
+++ server/routes/listing.js
@@ -4,6 +4,12 @@ const listingController = require('../controllers/listingController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+router.get('/', listingController.getAllListings, (req, res) => {
+  return res.status(200).json(res.locals.listings)
+});
+
+router.post('/', listingController.addListing, (req, res) => {
+  return res.status(200).json(res.locals.newListing);
+});
 
 module.exports = router;

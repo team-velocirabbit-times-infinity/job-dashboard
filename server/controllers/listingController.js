9d43b58f747e44cb408f746b9f5a8df10587e770
Andy Lam <axm.lam@gmail.com>
2023-11-04 14:00:03 -0700

    set up initial routes and controller skeleton

---
+++ server/controllers/listingController.js
@@ -0,0 +1,7 @@
+// const db = require('../models/listingModel');
+
+const listingController = {};
+
+listingController.getListings = (req, res, next) => {};
+
+module.exports = listingController;
---
+++ server/routes/listing.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const listingController = require('../controllers/listingController');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {});
+
+module.exports = router;

eaf91d1cfb3af11d65d75451b186fcdf21abf45d
Pegah Chendari <chendaripegah@gmail.com>
2023-11-05 12:02:39 -0800

    edited

--- client/src/containers/CategoryFilterContainer.jsx
+++ client/src/containers/CategoryFilterContainer.jsx
@@ -3,72 +3,67 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-// import Container from 'react-bootstrap/Container';
-
 
 const CategoryFilterContainer = () => {
-    const [activeButton, setActiveButton] = useState(null);
+  const [activeButton, setActiveButton] = useState(null);
 
-    const handleButtonClick = (buttonType) => {
-      setActiveButton(buttonType);
+  const handleButtonClick = (buttonLabel) => {
+    if (buttonLabel === activeButton) {
+      setActiveButton(null);
+    } else {
+      setActiveButton(buttonLabel);
+    }
+  };
 
-        // if (buttonType === activeButton) {
-        //   setActiveButton(null);
-        // } else {
-        //   setActiveButton(buttonType);
-        // }
-    };
-
-    return (
+  
+  return (
     <div>
-    <Form>
-        {/* <Form.Group>
-            <Form.Control as="input" type="text" placeholder="Filter by:" />
-        </Form.Group> */}
-            <Button variant="primary" type="button" onClick={() => handleButtonClick('Title')}>Title</Button>
-            <Button variant="primary" type="button" onClick={() => handleButtonClick('Company')}>Company</Button>
-            <Button variant="primary" type="button" onClick={() => handleButtonClick('Salary Range')}>Salary Range</Button>
-            <Button variant="primary" type="submit" onClick={() => handleButtonClick('Level')}>Level</Button>
-            <Button variant="primary" type="button" onClick={() => handleButtonClick('Status')}>Status</Button>
-    </Form>
-    {activeButton && (
+      <Form>
+        <Button variant="primary" type="button" onClick={() => handleButtonClick('Title')} >Title</Button>
+        <Button variant="primary" type="button" onClick={() => handleButtonClick('Company')} >Company</Button>
+        <Button variant="primary" type="button" onClick={() => handleButtonClick('Salary Range')} >Salary Range</Button>
+        <Button variant="primary" type="button" onClick={() => handleButtonClick('Level')} >Level</Button>
+        <Button variant="primary" type="button" onClick={() => handleButtonClick('Status')} >Status</Button>
+      </Form>
+
+      {activeButton && (
         <Card style={{ width: '18rem' }}>
           <ListGroup variant="flush">
-            {activeButton === 'Level' && ( 
-            <div>
+            {activeButton === 'Level' && (
+              <>
                 <ListGroup.Item>Beginner</ListGroup.Item>
                 <ListGroup.Item>Intermediate</ListGroup.Item>
                 <ListGroup.Item>Senior</ListGroup.Item>
                 <ListGroup.Item>Advanced</ListGroup.Item>
-            </div>
+              </>
             )}
 
             {activeButton === 'Salary Range' && (
-            <div> 
+              <>
                 <ListGroup.Item>Under 50K</ListGroup.Item>
                 <ListGroup.Item>50K To 100K</ListGroup.Item>
                 <ListGroup.Item>100K To 200K</ListGroup.Item>
                 <ListGroup.Item>Over 200K</ListGroup.Item>
-            </div> 
+              </>
             )}
 
             {activeButton === 'Status' && (
-            <div>
+              <>
                 <ListGroup.Item>None</ListGroup.Item>
                 <ListGroup.Item>Applied</ListGroup.Item>
-            </div> 
+              </>
             )}
 
             {activeButton === 'Company' && (
-            <div> 
+              <>
                 <ListGroup.Item>None</ListGroup.Item>
-            </div>
+              </>
             )}
 
-            {activeButton === 'Title' && ( 
-            <div>
+            {activeButton === 'Title' && (
+              <>
                 <ListGroup.Item>None</ListGroup.Item>
-            </div>
+              </>
             )}
           </ListGroup>
         </Card>
@@ -77,7 +72,4 @@ const CategoryFilterContainer = () => {
   );
 };
 
-
 export default CategoryFilterContainer;
-
-
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './styles.scss';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 render(<App />, document.getElementById('root'));

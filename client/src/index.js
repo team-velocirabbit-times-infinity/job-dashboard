694226ce846eebd619aea327d993aa794aa76286
Pegah Chendari <chendaripegah@gmail.com>
2023-11-05 14:26:06 -0800

    new code

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './styles.scss';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 render(<App />, document.getElementById('root'));
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles.scss';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));

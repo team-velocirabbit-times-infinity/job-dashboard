5fcaabab734d75fa27209ab4e295f01f5bc3455b
Connie Johnson <conniejohnson@Connies-Air.lan>
2023-11-07 19:35:14 -0600

    Installed Jest in devDep

--- _tests/t-servers.js
+++ _tests/t-servers.js
@@ -21,33 +21,58 @@ describe('checking server routes', () => {
     });
 
       // check if post functionality is returning a status of 200 with correct content type (app/json)
+      describe('POST', () => {
+        it('responds with 200 status and application/json content type', () => {
+          return request(server)
+            .get('/filter')
+            .expect('Content-Type', /application\/json/)
+            .expect(200);
+        });
     
       // check if post has the correct json data in the response body (newListing)
-    
+      });
+  
       // check if put functionality is returning a status of 200 with correct content type (app/json)
+      describe('PUT', () => {
+        it('responds with 200 status and application/json content type', () => {
+          return request(server)
+            .get('/filter')
+            .expect('Content-Type', /application\/json/)
+            .expect(200);
+        });
     
       // check if put has the correct json data in the response body (updatedListing)
-    
+
+      });
+  
       // check if delete functionality is returning a status of 200 with correct content type (app/json)
+      describe('DELETE', () => {
+        it('responds with 200 status and application/json content type', () => {
+          return request(server)
+            .get('/filter')
+            .expect('Content-Type', /application\/json/)
+            .expect(200);
+        });
     
       // check if delete has the correct json data in the response body (deletedListing)
 
-  });
+    })
 
-  // check if /filter path returns a status of 200 and with the expected content type (json)
-  describe('/filter', () => {
-    describe('GET', () => {
-      it('responds with 200 status and application/json content type', () => {
-        return request(server)
-          .get('/filter')
-          .expect('Content-Type', /application\/json/)
-          .expect(200);
+    // check if /filter path returns a status of 200 and with the expected content type (json)
+    describe('/filter', () => {
+      describe('GET', () => {
+        it('responds with 200 status and application/json content type', () => {
+          return request(server)
+            .get('/filter')
+            .expect('Content-Type', /application\/json/)
+            .expect(200);
+        });
+
+        // check to see if /filter path returns with a filterListing json object in the response body
+        it('filtered listings are returned in the response body', () => {
+
+        });
       });
-
-      // check to see if /filter path returns with a filterListing json object in the response body
-      it('filtered listings are returned in the response body', () => {
-
-      });
-    });
+    })
   })
 });

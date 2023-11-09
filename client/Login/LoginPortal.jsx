8a843d4ca7c7098cf5185a83c3cef2e50fe860d1
Harold Reeves <haroldreeves@gmail.com>
2023-11-09 11:43:45 -0800

    update > Co-authored-by: Lena <lenal.zhao@gmail.com> Co-authored-by: Connie <tenenoff071@hotmail.com> Co-authored-by: Rui <ruifan@indiana.edu> Co-authored-by: Harold <haroldreeves@gmail.com> Co-authored-by: William <william.b.kil@gmail.com>

--- client/Login/LoginPortal.jsx
+++ client/Login/LoginPortal.jsx
@@ -5,17 +5,3 @@ const LoginPortal = () => {
 };
 
 export default LoginPortal;
-
-// <div className='titleContainer'>LOGIN</div>
-//     <div className='userPassContainer'>
-//       <input type='text' placeholder='Username' className='usernameInput' />
-//       <input type='text' placeholder='password' className='passwordInput' />
-//       <div className='buttonContainer'>
-//         <button class='input-box' className='loginButton' id='loginButton'>
-//           Login
-//         </button>
-//         <button className='signupButton' id='signupButton'>
-//           Sign Up
-//         </button>
-//       </div>
-//     </div>

d9233510351a1ae637395a42e707d511d9ce4e5c
Rui Fan <ruifan@Ruis-MacBook-Air.local>
2023-11-09 17:51:01 -0800

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import LoginPortal from './containers/LoginPortal';
 import MainContainer from './containers/MainContainer';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <MainContainer />
+      <Routes>
+        <Route path='/a' element={<LoginPortal />} />
+        <Route path='/' element={<MainContainer />} />
+      </Routes>
     </div>
   );
 };
